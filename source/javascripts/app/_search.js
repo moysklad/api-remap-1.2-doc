c054523c42cd34e9a1c88d4e5addcea18cb00f43
strubitsyn <strubitsyn@moysklad.ru>
2022-07-01 20:30:37 +0300

    MC-44347 publish method buildDocumentsForSearch

--- source/javascripts/app/_search.js
+++ source/javascripts/app/_search.js
@@ -12,34 +12,38 @@ $(function () {
   let searchDelay = 0;
   let timeoutHandle = 0;
 
-  let headerSelector = 'h1, h2, h3';
-  let links = $(headerSelector).map(function() {
-    let title = $(this);
-    let body = title.nextUntil(headerSelector, ':not(.highlight):not(button)');
-    let bodyTexts = body.toArray().flatMap(function (node) {
-      let result = [];
-      let currentTextNode;
-      let itr = document.createNodeIterator(node, NodeFilter.SHOW_TEXT)
-      while ((currentTextNode = itr.nextNode())) {
-        let cleanedText = currentTextNode.nodeValue
-          .replaceAll('https://online.moysklad.ru/api/remap/1.2/', ' ')
-          .replaceAll(/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/ig, ' ')
-          .replaceAll(/\s+/ig, ' ')
-          .trim();
-        if(cleanedText !== "")
-          result.push(cleanedText);
-      }
-      return result;
-    }).join(" ");
+  document.buildDocumentsForSearch = function () {
+    let headerSelector = 'h1, h2, h3';
+    return $(headerSelector).map(function () {
+      let title = $(this);
+      let body = title.nextUntil(headerSelector, ':not(.highlight):not(button)');
+      let bodyTexts = body.toArray().flatMap(function (node) {
+        let result = [];
+        let currentTextNode;
+        let itr = document.createNodeIterator(node, NodeFilter.SHOW_TEXT)
+        while ((currentTextNode = itr.nextNode())) {
+          let cleanedText = currentTextNode.nodeValue
+            .replaceAll('https://online.moysklad.ru/api/remap/1.2/', ' ')
+            .replaceAll(/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/ig, ' ')
+            .replaceAll(/\s+/ig, ' ')
+            .trim();
+          if (cleanedText !== "")
+            result.push(cleanedText);
+        }
+        return result;
+      }).join(" ");
 
-    return {
-      "id": title.prop('id'),
-      "title": title.text(),
-      "body": bodyTexts
-    };
-  }).toArray();
+      return {
+        "id": title.prop('id'),
+        "title": title.text(),
+        "body": bodyTexts
+      };
+    }).toArray();
+  }
 
-  var index = lunr(function () {
+  let links = document.buildDocumentsForSearch();
+
+  let index = lunr(function () {
     this.use(lunr.multiLanguage('ru', 'en'));
     this.ref('id');
     this.tokenizer.separator = /\W/;
